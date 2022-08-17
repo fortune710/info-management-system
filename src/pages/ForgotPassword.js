1a3d2b794cf776cce63a41584b961add672d7421
fortune710 <fortune0208@yahoo.com>
2022-08-17 13:44:05 +0100

    made page to use MUI textfield

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,12 +1,29 @@
-import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
-import Firebase from '../firebase-init';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../utils/firebase.utils';
+
 import { useForm } from 'react-hook-form';
 
+import { Button, TextField, Modal, Typography, Box, Snackbar, IconButton } from "@mui/material";
 
-import FormInput from '../components/FormInput/FormInput';
-import Modal from '../components/Modal/Modal';
 import React, { useState } from 'react';
+import { Close } from '@mui/icons-material';
 
+
+const boxStyle = {
+    background: '#fff',
+    border: '2px solid #000',
+    width: 400
+}
+
+const modalStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}
+
+const toastStyle = {
+    margin: '0 auto'
+}
 
 const ForgotPasswordPage = () => {
     const { register, handleSubmit, getValues } = useForm({
@@ -16,29 +33,73 @@ const ForgotPasswordPage = () => {
     });
 
     const submitEmail = () => {
-        const {email} = getValues();
-        setModalOpen(true)
+        const { email } = getValues();
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            setModalOpen(true)
+        })
+        .catch((err) => {
+            if(err.code === 'auth/network-request-failed'){
+                setToastOpen(true)
+            }
+        })
+
     }
 
     const [modalOpen, setModalOpen] = useState(false)
+    const [toastOpen, setToastOpen] = useState(false)
 
     return(
         <React.Fragment>
             
-        <main>
-            <form onSubmit={handleSubmit()}>
-                <FormInput
-                    register={register}
+        <main className='forgot-password-page'>
+            <h1>Forgot Password</h1>
+            <form onSubmit={handleSubmit(submitEmail)}>
+                <TextField 
+                    {...register('email')}
                     placeholder="Enter the email for this account"
                     type="email"
                     name={'email'}
-                    />
+                    label="Email"
+                />
 
-                <button type='submit'>
+                <Button variant="contained" type='submit'>
                     Submit
-                </button>
+                </Button>
             </form>
 
+            <Modal
+                sx={modalStyle}
+                open={modalOpen}
+                onClose={() => setModalOpen(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={boxStyle}>
+                    <Typography id="modal-modal-title" component="h2">
+                        Check Your Email
+                    </Typography>
+                    <Typography id="modal-modal-description">
+                        We've sent you a link to help you reset 
+                        your email.
+                    </Typography>
+                </Box>
+            </Modal>
+
+
+
+            <Snackbar
+                sx={toastStyle}
+                open={toastOpen}
+                onClose={() => setToastOpen(false)}
+                autoHideDuration={3000}
+                message="Not connected to the Internet"
+                action={
+                    <IconButton onClick={() => setToastOpen(false)}>
+                        <Close/>
+                    </IconButton>
+                }
+            />
           
 
         </main>
