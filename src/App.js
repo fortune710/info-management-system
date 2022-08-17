d02be5d5553f3ba21a5bb6fc5851ae18cdb5ff96
fortune710 <fortune0208@yahoo.com>
2022-08-17 13:41:09 +0100

    added a new route ↗️

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import ForgotPasswordPage from './pages/ForgotPassword';
 import MainAreaPage from './pages/Dashboard/Main';
 import SignInPage from './pages/SignIn/SignIn';
 
-import HomePage from "./pages/main-pages/Home"
+import HomePage from "./pages/main-pages/Home/Home"
 import InventoryPage from "./pages/main-pages/Inventory/Inventory";
 import StaffPage from "./pages/main-pages/StaffPage/Staff";
 import SettingsPage from "./pages/main-pages/Settings";
 
+import AddStaffPage from './pages/main-pages/StaffPage/AddStaff';
+import AllStaffPage from './pages/main-pages/StaffPage/AllStaff';
+
 import "./theme/variables.scss";
 import './App.scss';
-import AddStaffPage from './pages/main-pages/StaffPage/AddStaff';
 
 function App() {
   return (
@@ -22,6 +24,7 @@ function App() {
         <Route path="inventory" element={<InventoryPage/>} />
         <Route path="staff" element={<StaffPage/>} />
         <Route path='add-staff' element={<AddStaffPage/>} />
+        <Route path="all-staff" element={<AllStaffPage/>} />
         <Route path="settings" element={<SettingsPage/>} />
       </Route>
       <Route exact path='/forgot-password' element={<ForgotPasswordPage/>} />
