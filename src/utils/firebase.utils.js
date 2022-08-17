866b566efdc5e0be86d470e3aff9230cf9b947a4
fortune710 <fortune0208@yahoo.com>
2022-08-17 13:42:44 +0100

    renamed firebase config and added exports

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +20,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const Firebase = initializeApp(firebaseConfig);
 const analytics = getAnalytics(Firebase);
+
+export const auth = getAuth(Firebase);
+export const firestore = getFirestore(Firebase);
 
 export default Firebase;
